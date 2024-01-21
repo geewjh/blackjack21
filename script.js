3f3669218e8988ad5407ca3ac7d2893a65c02144
geewjh <geewjh@gmail.com>
2024-01-21 20:14:29 +0800

    edited global variables, gave better naming convenetions and added buildDeck() and shuffleDeck()

--- script.js
+++ script.js
@@ -15,21 +15,39 @@ const values = [
   "Q",
   "K",
 ];
+
 const messages = {
   win: "dollar dollar bill 💰.",
   lose: "you win some, you lose some.",
-  playerBJ: "you have just served a nice warm 🍽️ of blackjack to the banker.",
+  playerBJ: "winner winner chicken dinner",
   playerBB: "you have won x 3 bet.",
-  playerTrips7: "you have lost x 7 bet.",
+  player777: "you have won x 7 bet.",
+  bankerBJ: "you have been served a nice warm 🍽️ of blackjack from the banker.",
+  bankerBB: "you have lost x 3 bet.",
+  banker777: "you have lost x 7 bet.",
   insufficientBal: "insufficient balance.",
   push: "good fight.",
   cannotDeal: "min. bet to play is $5",
 };
 
 /*----- state variables -----*/
-const deck = {};
-const handPoints = {};
-const gameState = {};
+const deck = {
+  shuffled: [],
+  bankerCards: [],
+  playerCards: [],
+};
+
+const handPoints = {
+  playerHP: [],
+  bankerHP: [],
+};
+
+const gameState = {
+  gameOver: false,
+  playerBlackJack: false,
+  playerBanBan: false,
+  playerTrips7: false,
+};
 
 const bettingInfo = {
   balance: 1000,
@@ -37,38 +55,67 @@ const bettingInfo = {
 };
 
 /*----- cached elements  -----*/
-const stateEl = {
+const results = {
   bankerPoints: document.querySelector("#banker-points"),
   playerPoints: document.querySelector("#your-points"),
   resultMessage: document.querySelector("#result-message"),
 };
 
-const cardsEl = {
+const points = {
   banker: document.querySelector("#banker-points-container"),
   player: document.querySelector("#player-points-container"),
 };
 
-const chipEl = {
+const chips = {
   bet5: document.querySelector("#bet-5"),
   bet10: document.querySelector("#bet-10"),
   bet25: document.querySelector("#bet-25"),
   bet50: document.querySelector("#bet-50"),
 };
 
-const bettingEl = {
+const inputChoices = {
   undo: document.querySelector("#undo-button"),
-  repeat: document.querySelector("#repeat-button"),
-  clear: document.querySelector("#clear-button"),
   deal: document.querySelector("#deal-button"),
 };
 
-const ingameEl = {
+const inGameChoices = {
   double: document.querySelector("#double-button"),
   hit: document.querySelector("#hit-button"),
   stand: document.querySelector("#stand-button"),
   run: document.querySelector("#run-button"),
 };
-
 /*----- event listeners -----*/
 
 /*----- functions -----*/
+
+function buildDeck() {
+  let deckOfCards = [];
+
+  for (let suit of suits) {
+    for (let value of values) {
+      let point = parseInt(value);
+      if (value === "A") {
+        point = 11;
+      } else if (isNaN(value)) {
+        point = 10;
+      }
+      deckOfCards.push({
+        face: `${value}-${suit}`,
+        point: point,
+      });
+    }
+  }
+  return deckOfCards;
+}
+
+function shuffleDeck() {
+  deckOfCards = buildDeck();
+
+  while (deckOfCards.length > 0) {
+    const randomIndex = Math.floor(Math.random() * deckOfCards.length);
+    const [removedCard] = deckOfCards.splice(randomIndex, 1);
+    deck.shuffled.push(removedCard);
+  }
+}
+
+shuffleDeck();
